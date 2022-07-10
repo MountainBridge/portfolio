bb2c766bfe93e0ff343271ae332f5eba3142f535
MountainBridges <nidhi199.verma@gmail.com>
2022-07-10 17:28:59 +0530

    portfolio

---
+++ Projects/portfolio/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <span class="circle">
+        <img src={require('./assets/girl_remote.gif')}/>
+        </span>
+        <p class="font"><b>Hello, my name is Nidhi. I am a web developer and designer based out of India.</b></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
