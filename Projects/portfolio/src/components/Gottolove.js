1551759ce1f8515d011c50039fb79bf4bfc2f6dd
MountainBridges <nidhi199.verma@gmail.com>
2022-08-02 11:06:03 +0530

    add algorithm input

--- Projects/portfolio/src/components/Gottolove.js
+++ Projects/portfolio/src/components/Gottolove.js
@@ -1,11 +1,23 @@
 import './gottolove.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Input = () => <input placeholder = "Add Algorithm here"/>;
+
+const Form = () => [inputList, setInputList] = useState([]);
+
+const onAddButtonClick = event => setInputList(inputList.concat(<Input key = {inputList.length}/>));
 
 function Gottolove() {
-  return (
-<div class="card" id="Gottolove">
-<p>Data Structures and Algorithms</p>
-</div>
-);
+return (
+  <div class="card" id="Gottolove">
+    <h1>Data Structures and Algorithms</h1>
+    <React.Fragment>
+    <button onClick = {onAddButtonClick}>Add Algorithm</button>
+      {inputList}
+    </React.Fragment>
+  </div>
+  );
 }
 
 export default Gottolove;
